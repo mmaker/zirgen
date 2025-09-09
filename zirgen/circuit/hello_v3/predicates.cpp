069e02bf3bb680c489ae215530285e5b2278e9dc
jacobdweightman <jacob@risczero.com>
2025-09-09 17:42:04 -0500

    clang-format

--- zirgen/circuit/hello_v3/predicates.cpp
+++ zirgen/circuit/hello_v3/predicates.cpp
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "zirgen/circuit/hello_v3/CircuitInterface.h"
 #include "zirgen/circuit/predicates/predicates.h"
+#include "zirgen/circuit/hello_v3/CircuitInterface.h"
 #include "zirgen/circuit/recursion/code.h"
 #include "zirgen/circuit/verify/verify.h"
 #include "zirgen/compiler/codegen/codegen.h"
